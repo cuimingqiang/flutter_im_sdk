b0798802eb1a1298c9b0e750c4f120d00b4ad7e8
cuimingqiang <770460131@qq.com>
2020-12-22 15:25:07 +0800

    init commit

---
+++ android/src/main/java/com/easemob/im_flutter_sdk/Func.java
@@ -0,0 +1,5 @@
+package com.easemob.im_flutter_sdk;
+
+public interface Func<T>{
+    void  accept(T t);
+}
---
+++ ios/Classes/ImFlutterSdkPlugin.h
@@ -0,0 +1,6 @@
+#import <Flutter/Flutter.h>
+
+@interface ImFlutterSdkPlugin : NSObject<FlutterPlugin>
++(void)setDeivceToken:(NSData *)aDeviceToken;
+@end
+
